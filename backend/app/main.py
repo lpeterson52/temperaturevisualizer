cd97c48bd339a35b0bd18e83cdafd7ad166c2f52
Lucas Peterson <111457379+lpeterson52@users.noreply.github.com>
2025-07-16 14:03:41 -0700

    added cors header

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from typing import Union
 import asyncio
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.cache import refresh_cache, router as cache_router
 from app.api.routes import router as merge_router
 
@@ -12,6 +13,14 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(merge_router, prefix="/api")
 app.include_router(cache_router, prefix="/cache")
